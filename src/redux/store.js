bd9ed5185746a65d321187b4c5a74cc9451d1f53
sofiaCocker <sonya.cocker.2006@gmail.com>
2021-06-22 00:37:04 +0300

    fixed store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -30,6 +30,9 @@ export const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
   )
 );
 
